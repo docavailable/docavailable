8d1453ac968fa46e1a19370ff9b969e7be02e597
sleeky <161637665+sleekyb@users.noreply.github.com>
2025-09-06 13:18:39 +0200

    feat: add migrations back and clean up
    - Add php artisan migrate --force to startup command
    - Remove test.php file since it's no longer needed
    - Migrations will run automatically on each deployment
    - Full Laravel app with database setup is now deployed

--- backend/public/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo "Hello from DigitalOcean!";
-echo "<br>PHP Version: " . phpversion();
-echo "<br>Server Time: " . date('Y-m-d H:i:s');
-echo "<br>Port: " . ($_SERVER['SERVER_PORT'] ?? 'unknown');
-phpinfo();
-?>
